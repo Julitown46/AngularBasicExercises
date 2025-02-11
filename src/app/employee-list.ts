e63e40b72c7f50d4aff3174f0f7d2acf6d23a713
Julitown46 <morenojulian4502@gmail.com>
2025-02-11 19:11:43 +0100

    Terminado por fin todo

---
+++ src/app/employee-list.ts
@@ -0,0 +1,12 @@
+import { Employee } from "./model/Employee";
+
+export const EMPLOYEE_LIST: Employee[] = [
+    { name: 'John Doe', position: 'Designer',salary: 1500, department: 'Design'},
+    { name: 'Jane Smith', position: 'Developer',salary: 1200, department: 'IT'},
+    { name: 'Bob Johnson', position: 'Manager',salary: 3000, department: 'HR'},
+    { name: 'Mary Brown', position: 'Secretary', salary: 1300, department: 'Administration'},
+    { name: 'James Lee', position: 'Designer', salary: 1600, department: 'Design'},
+    { name: 'Linda Wilson', position: 'Manager', salary: 3500, department: 'HR'},
+    { name: 'David Davis', position: 'Developer', salary: 1250, department: 'IT'},
+    { name: 'Ana White', position: 'Secretary', salary: 1350, department: 'Administration'},    
+];
--- src/app/ex09/ex09.component.ts
+++ src/app/ex09/ex09.component.ts
@@ -1,34 +1,40 @@
 import { Component } from '@angular/core';
+import { Employee } from "../model/Employee";
+import { EMPLOYEE_LIST } from "../employee-list";
+import { NgForOf } from "@angular/common";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-ex09',
-  imports: [],
-  templateUrl: './ex09.component.html',
-  styleUrl: './ex09.component.css'
-})
-export class Ex09Component {
-
-}
-
-/* import { Component } from '@angular/core';
-import {Employee} from "../employee";
-import {EMPLOYEE_LIST} from "../employee-list";
-import {NgForOf} from "@angular/common";
+type emplKey = keyof Employee;
 
 @Component({
   selector: 'app-ex09',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './ex09.component.html'
 })
+
 export class Ex09Component {
   employees: Employee[] = [];
+  sortingCriteria:emplKey = 'name';
 
   constructor() { }
 
   ngOnInit(): void {
     this.employees = EMPLOYEE_LIST;
   }
-} */
+
+  sortEmployees(sortCriteria: string): void {
+    console.log(sortCriteria);
+
+    let key: emplKey = this.sortingCriteria;
+    console.log(key);
+
+    this.employees.sort((a: Employee, b: Employee) => String(b[key]).localeCompare(String(a[key])));
+
+  }
+}
